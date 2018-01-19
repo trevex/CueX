9b89b93cd9b5d8377e0f366ff87abd406e1b236d
Nik Voss <niklas.voss@gmail.com>
2018-01-19 18:11:47 +0100

    Basic output of docx.

--- CueX.Core/SpatialGrainState.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Numerics;
-
-namespace CueX.Core
-{
-    public class SpatialGrainState<TPrecision> where TPrecision : struct
-    {
-        public Vector<TPrecision> Position;
-    }
-}
--- Documentation/articles/toc.md
+++
@@ -1,3 +0,0 @@
-# [Getting Started](docfx_getting_started.md)
-# [Engineering Guidelines](engineering_guidelines.md)
-# [CSharp Coding Standards](csharp_coding_standards.md)
