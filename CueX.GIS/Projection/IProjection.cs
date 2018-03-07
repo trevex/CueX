4abbdbd2d70b2ac42654c790e7c5b71800b5d972
Niklas Voss <niklas.voss@gmail.com>
2018-03-07 09:19:44 +0100

    Moved projections into GIS project.

--- CueX.GIS/Projection/IProjection.cs
+++ CueX.GIS/Projection/IProjection.cs
@@ -2,6 +2,9 @@
 // Licensed under the Apache2 license. See LICENSE file in the project root for full license information.
 namespace CueX.Numerics.Projection
 {
+    /// <summary>
+    /// A projection describes how GIS data is mapped into 2-dimensional space and how to calculate distances, translation et cetera. 
+    /// </summary>
     public interface IProjection
     {
         
